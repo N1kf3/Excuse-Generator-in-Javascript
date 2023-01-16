53ade304fff2db0d74901fd49caaa0244ab676c0
N1kf3 <wilfredo.andrade92@gmail.com>
2023-01-16 18:49:00 +0000

    segunda version

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function generate() {
+  let who_num = Math.floor(Math.random() * 3);
+  let action_num = Math.floor(Math.random() * 3);
+  let what_num = Math.floor(Math.random() * 2);
+  let when_num = Math.floor(Math.random() * 5);
+  let new_frase =
+    who[who_num] +
+    " " +
+    action[action_num] +
+    " " +
+    what[what_num] +
+    " " +
+    when[when_num];
+  document.querySelector("#excuse").innerHTML = new_frase;
 };
